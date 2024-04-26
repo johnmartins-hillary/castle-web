be41b41ea49fa025fd7e119902e3a6f4c8e27bb7
Ebubechi Ihediwa <ebube2003nice@gmail.com>
2024-04-26 22:56:17 +0100

    Updated base_url

--- services/search/get-users.ts
+++ services/search/get-users.ts
@@ -4,74 +4,88 @@ import { setSingleUser } from "@/redux/slices/single-user";
 import { setUsersHandler } from "@/redux/slices/users";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 interface ApiResponse<T> {
-    data: T
+  data: T;
 }
 
 interface Prop {
-    data?: any
+  data?: any;
 }
 export const usersApi = createApi({
-    reducerPath: "usersApi",
-    tagTypes: ['users', 'single-users', 'searched-users'],
-    baseQuery: fetchBaseQuery({
-        baseUrl: `${BASE_URL}`,
+  reducerPath: "usersApi",
+  tagTypes: ["users", "single-users", "searched-users"],
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${BASE_URL}`,
+    mode: "cors",
+    prepareHeaders: (headers, { getState }) => {
+      const token = localStorage.getItem("authorization") || null;
+      if (token) {
+        headers.set("Authorization", `Bearer ${JSON.parse(token)}`);
+      }
+      return headers;
+    }
+  }),
+  endpoints: builder => ({
+    getUsers: builder.query<void, AllUsersProps>({
+      query: ({ page_no }) => ({
+        url: `users?page=${page_no}`,
+        method: "GET",
         mode: "cors",
-        prepareHeaders: (headers, { getState }) => {
-            const token = localStorage.getItem("authorization") || null
-            if (token) {
-                headers.set("Authorization", `Bearer ${JSON.parse(token)}`)
-            }
-            return headers;
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          Accept: "application/json"
         }
+      }),
+      providesTags: ["users"]
     }),
-    endpoints: (builder) => (
-        {
-            getUsers: builder.query<void, AllUsersProps>({
-                query: ({ page_no }) => ({
-                    url: `users?page=${page_no}`,
-                    method: "GET",
-                    mode: "cors",
-                    headers: {
-                        'Content-type': 'application/json; charset=UTF-8',
-                        'Accept': 'application/json'
-                    }
-                }),
-                providesTags: ['users']
-            }),
 
-            getSingleUser: builder.query<void, SingleUserProps>({
-                query: ({ id }) => ({
-                    url: `user/${id}`,
-                    method: "GET",
-                    mode: "cors",
-                    headers: {
-                        'Content-type': 'application/json; charset=UTF-8',
-                        'Accept': 'application/json'
-                    }
-                }),
-                async onQueryStarted(args, { dispatch, queryFulfilled }) {
-                    try {
-                        const { data } = await queryFulfilled;
-                        dispatch(setSingleUser(data))
-                    } catch (error) {
-                        console.log("Single User error", error)
-                    }
-                }
-            }),
-            searchUsers: builder.query<void, AllUsersProps>({
-                query: ({ keyword }) => ({
-                    url: `users/search?queries=${keyword}`,
-                    method: "GET",
-                    mode: "cors",
-                    headers: {
-                        'Content-type': 'application/json; charset=UTF-8',
-                        'Accept': 'application/json'
-                    }
-                }),
-            })
+    getSingleUser: builder.query<void, SingleUserProps>({
+      query: ({ id }) => ({
+        url: `user/${id}`,
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          Accept: "application/json"
         }
-    )
-})
+      }),
+      async onQueryStarted(args, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          dispatch(setSingleUser(data));
+        } catch (error) {
+          console.log("Single User error", error);
+        }
+      }
+    }),
+    searchUsers: builder.query<void, AllUsersProps>({
+      query: ({ keyword }) => ({
+        url: `users/search?queries=${keyword}`,
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          Accept: "application/json"
+        }
+      })
+    }),
+    searchUsersByCategory: builder.query<void, { cat: any }>({
+      query: ({ cat }) => ({
+        url: `users/category`,
+        method: "GET",
+        mode: "cors",
+        params: { cat },
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          Accept: "application/json"
+        }
+      })
+    })
+  })
+});
 
-
-export const { useGetUsersQuery, useGetSingleUserQuery, useSearchUsersQuery } = usersApi
+export const {
+  useGetUsersQuery,
+  useGetSingleUserQuery,
+  useSearchUsersQuery,
+  useLazySearchUsersByCategoryQuery
+} = usersApi;
