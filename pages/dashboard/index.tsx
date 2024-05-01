c85519969cd45cfa707ce228b628be34d351fcc4
Ebubechi Ihediwa <ebube2003nice@gmail.com>
2024-05-01 18:05:54 +0100

    reverting back

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -21,7 +21,7 @@ const Dashboard = () => {
             </div> */}
            <DashboardBanner/>
         </div>
-        {/* <BookingHistory/> */}
+        <BookingHistory/>
         </DashboardLayput>
         </>
      );
