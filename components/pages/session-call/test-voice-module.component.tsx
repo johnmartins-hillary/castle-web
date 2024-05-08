17ddfc206899cffbe6cc13225ba59dbcd59574cd
Ebubechi Ihediwa <ebube2003nice@gmail.com>
2024-05-08 16:19:46 +0100

    More test on call

--- components/pages/session-call/test-voice-module.component.tsx
+++ components/pages/session-call/test-voice-module.component.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import Peer from "peerjs-next";
 import { Input } from "@/components/ui/input";
-
 const TestVoiceCallModule = () => {
   const currentStreamRef = useRef<any>();
   const remoteStreamRef = useRef<any>();
@@ -9,6 +8,7 @@ const TestVoiceCallModule = () => {
   const [roomId, setId] = useState("");
   let media: any;
 
+  const getUserMedia = media?.getUserMedia;
   useEffect(() => {}, []);
 
   const callHandler = () => {
@@ -22,29 +22,21 @@ const TestVoiceCallModule = () => {
           ?.getUserMedia({ audio: true })
           .then((stream: any) => {
             localStream = stream;
-            // Mute the local audio playback for the caller
-            localStream.getAudioTracks().forEach((track: MediaStreamTrack) => {
-              track.enabled = false;
-            });
             currentStreamRef.current.srcObject = stream;
+            currentStreamRef.current.volume = 0;
             currentStreamRef.current.play();
           })
           .catch((err: any) => {
-            console.log("Error getting current stream", err);
+            console.log("Error getting ccurrent stream", err);
           });
       });
 
       peerInstance.on("call", (call) => {
-        navigator.mediaDevices
-          ?.getUserMedia({ audio: true })
-          .then((stream: any) => {
-            remoteStreamRef.current.srcObject = stream;
-            remoteStreamRef.current.play();
-            call.answer(stream);
-          })
-          .catch((err: any) => {
-            console.log("Error getting remote stream", err);
-          });
+        call.answer(localStream);
+        call.on("stream", (stream: any) => {
+          remoteStreamRef.current.srcObject = stream;
+          remoteStreamRef.current.play();
+        });
         setCurrentPeer(call);
       });
     }
@@ -59,29 +51,27 @@ const TestVoiceCallModule = () => {
           ?.getUserMedia({ audio: true })
           .then((stream: any) => {
             localStream = stream;
-            // Mute the local audio playback for the callee
-            localStream.getAudioTracks().forEach((track: MediaStreamTrack) => {
-              track.enabled = false;
-            });
-            currentStreamRef.current.srcObject = stream;
-            currentStreamRef.current.play();
+            // currentStreamRef.current.srcObject = stream
+            // currentStreamRef.current.play()
             let call = peerInstance.call(roomId, stream);
-            call.on("stream", (remoteStream: any) => {
-              remoteStreamRef.current.srcObject = remoteStream;
+            call.on("stream", () => {
+              remoteStreamRef.current.srcObject = stream;
               remoteStreamRef.current.play();
             });
             setCurrentPeer(call);
           })
           .catch((err: any) => {
-            console.log("Error getting local stream", err);
+            console.log("Error getting ccurrent stream", err);
           });
       });
     }
   };
-
   return (
     <>
       <div className="w-full flex items-center justify-center gap-3 mt-24 ">
+        {/* <SpeakeronIcon onClick={()=>{}} size={35} className={'bg-black p-2 rounded-sm'} color={'white'} />
+            <MicIcon size={27} /> */}
+
         <Input
           className=" border-black border"
           type="text"
@@ -93,7 +83,7 @@ const TestVoiceCallModule = () => {
         <button onClick={callHandler}>Call</button>
         <button onClick={answerCall}>Answer</button>
 
-        <audio ref={currentStreamRef} muted={true}></audio>
+        <audio ref={currentStreamRef}></audio>
         <audio ref={remoteStreamRef}></audio>
       </div>
     </>
